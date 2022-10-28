138caa108955fe0d86dbb333f0559c19a2aa4e4b
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2022-10-28 23:53:25 +0200

    1 частина
    створення та рендеринг галереї

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,19 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryItemsRef = document.querySelector('.gallery__item');
+const imageGallery = document.querySelector('.gallery__link');
 
-console.log(galleryItems);
+galleryItemsRef.classList.add('gallery');
+
+const creatListMarkup = galleryItems.map(({
+  preview,
+  original,
+  description
+}) => `
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>`).join('');
+
+galleryItemsRef.insertAdjacentHTML('beforeend', creatListMarkup);
 
 
+
+console.log(galleryItemsRef)
+
+// imageGallery.addEventListener('submit', onClick);
+
+// function onClick(evt) {
+//   evt.preventDefault();
+// }
 
 // -------------------------------------------------------------------------------------------------------------------
 // Завдання 1 - галерея зображень
 // Створи галерею з можливістю кліку по її елементах і перегляду повнорозмірного зображення 
 // у модальному вікні.Подивися демо відео роботи галереї.
+
 // Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+
 // Реалізація делегування на div.gallery і отримання url великого зображення.
+
 // Підключення скрипту і стилів бібліотеки модального вікна basicLightbox.Використовуй CDN сервіс jsdelivr і 
 // додай у проект посилання на мініфіковані(.min) файли бібліотеки.
+
 // Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
+
 // Заміна значення атрибута src елемента < img > в модальному вікні перед відкриттям.Використовуй готову розмітку 
 // модального вікна із зображенням з прикладів бібліотеки basicLightbox.
 
@@ -32,7 +63,7 @@ console.log(galleryItems);
 //   </a>
 // </div>
 
-// Зверни увагу на те, що зображення обгорнуте посиланням, отже по кліку за замовчуванням користувач буде 
+// *onSearch*Зверни увагу на те, що зображення обгорнуте посиланням, отже по кліку за замовчуванням користувач буде 
 // перенаправлений на іншу сторінку.Заборони цю поведінку за замовчуванням.
 
 // Закриття з клавіатури
